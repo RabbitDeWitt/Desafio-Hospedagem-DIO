7d37e85e7696de01da9bc5ea5560b528682aa16d
RabbitDeWitt <davidsocoelho@outlook.com>
2024-09-07 22:34:31 -0300

    Desafio resolvido

---
+++ Interfaces/IReserva.cs
@@ -0,0 +1,12 @@
+﻿using DesafioProjetoHospedagem.Models;
+
+namespace DesafioProjetoHospedagem.Interfaces
+{
+    public interface IReserva
+    {
+        void CadastrarHospedes(List<Pessoa> hospedes);
+        void CadastrarSuite(Suite suite);
+        int ObterQuantidadeHospedes();
+        decimal CalcularValorDiaria();
+    }
+}
